8e1d1b54aa8481439d06f50467394e019b0e70cf
Aryan abrol <107027062+AryanAbrol@users.noreply.github.com>
2023-12-16 13:44:35 +0000

    variables

--- 01/01-variables.js
+++ 01/01-variables.js
@@ -3,7 +3,7 @@ let accName="king"
 var accCity="tumkur"
 accNo="1244234"
 let r
-
+//prefer not to use var because it is having scope issues
 accName="op"
 console.log(accName);
 console.table([accId,accName,accCity,accNo,r])
